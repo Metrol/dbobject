4f28f3ac98a80f5b4f09b56fb0351e76c724c333
Michael Collette <metrol@metrol.net>
2016-07-10 11:03:15 -0500

    Moves what had been Item to become DBObject

--- src/DBObject.php
+++ src/DBObject.php
@@ -6,17 +6,18 @@
  * @copyright (c) 2016, Michael Collette
  */
 
-
 namespace Metrol;
 
 use Metrol\DBTable;
 use Metrol\DBSql;
+use PDO;
 
 /**
- * Describes the functionality that all DBObjects must be able to support.
+ * Maps itself to a database record for the purposes of creating, updating,
+ * and deleting the information.
  *
  */
-interface DBObject
+class DBObject implements \JsonSerializable
 {
     /**
      * Flag set to specify that a load() has been attempted, and was successful
@@ -43,107 +44,495 @@ interface DBObject
     const NOT_FOUND = 86;
 
     /**
-     * Set a value for a field
+     * The database table that this item will be acting as a front end for
      *
-     * @param string $fieldName
+     * @var DBTable
+     */
+    protected $_objTable;
+
+    /**
+     * The database connection used to talk to this object
+     *
+     * @var PDO
+     */
+    protected $_objDb;
+
+    /**
+     * The record data for this object in key/value pairs
+     *
+     * @var array
+     */
+    protected $_objData;
+
+    /**
+     * Tracks the load status for the item
+     *
+     * @var integer
+     */
+    protected $_objLoadStatus;
+
+    /**
+     * Instantiate the object
+     *
+     * @param DBTable $table
+     * @param PDO     $databaseConnection
+     *
+     */
+    public function __construct(DBTable $table, PDO $databaseConnection)
+    {
+        $this->_objTable      = $table;
+        $this->_objDb         = $databaseConnection;
+        $this->_objData       = array();
+        $this->_objLoadStatus = self::NOT_LOADED;
+    }
+
+    /**
+     *
+     * @param string $field
+     *
+     * @return mixed|null
+     */
+    public function __get($field)
+    {
+        return $this->get($field);
+    }
+
+    /**
+     * @param string $field
+     * @param mixed $value
+     *
+     * @return $this
+     */
+    public function __set($field, $value)
+    {
+        return $this->set($field, $value);
+    }
+
+    /**
+     * @param string $field
+     *
+     * @return boolean
+     */
+    public function __isset($field)
+    {
+        $rtn = false;
+
+        if ( isset($this->_objData[$field]) )
+        {
+            $rtn = true;
+        }
+
+        return $rtn;
+    }
+
+    /**
+     * Provide the object data to support json_encode
+     *
+     * @return array
+     */
+    public function jsonSerialize()
+    {
+        return $this->_objData;
+    }
+
+    /**
+     *
+     * @param string $field
+     *
+     * @return mixed|null
+     */
+    public function get($field)
+    {
+        $rtn = null;
+
+        if ( !$this->_objTable->fieldExists($field) )
+        {
+            return null;
+        }
+
+        if ( isset($this->_objData[$field]) )
+        {
+            $rtn = $this->_objData[$field];
+        }
+
+        return $rtn;
+    }
+
+    /**
+     *
+     * @param string $field
      * @param mixed  $value
      *
      * @return $this
      */
-    public function set($fieldName, $value);
+    public function set($field, $value)
+    {
+        if ( !$this->_objTable->fieldExists($field) )
+        {
+            return $this;
+        }
+
+        $fieldObj = $this->getDBTable()->getField($field);
+
+        $this->_objData[$field] = $fieldObj->getPHPValue($value);
+
+        return $this;
+    }
 
     /**
-     * Get a value from a field
+     * Provides the primary key field for this object
      *
-     * @param string $fieldName
-     *
-     * @return mixed|null
+     * @return string|null
      */
-    public function get($fieldName);
+    public function getPrimaryKeyField()
+    {
+        $rtn = null;
+
+        $keys = $this->_objTable->getPrimaryKeys();
+
+        if ( count($keys) > 0 )
+        {
+            $rtn = $keys[0];
+        }
+
+        return $rtn;
+    }
 
     /**
-     * Initiates a query to the database to pull in a record.  This is entirely
-     * based on the primary key value.  If one is not provided, the value
-     * already stored in this object will be used.
-     *
-     * @param mixed $primaryKeyValue
-     *
-     * @return $this
-     *
-     * @throws \UnderflowException If no primary key is specified.
-     */
-    public function load($primaryKeyValue = null);
-
-    /**
-     * Loads a single record based on the WHERE criteria and optional bindings
-     * provided.
-     *
-     * Any value that is to be bound must have a quesion mark "?" used as a
-     * place holder.
-     *
-     * @param string $where
-     * @param mixed|array  $bindings
-     *
-     * @return $this
-     */
-    public function loadFromWhere($where, $bindings = null);
-
-    /**
-     * Will update a record if a primary key value has been set and the object
-     * has been marked as "Loaded".  Otherwise, a new record will be written.
-     *
-     * Any time a save is called, the values in this object will be updated.
-     * For new records, check getId() for automatically generated primary key
-     * values.
-     *
-     * @return $this
-     */
-    public function save();
-
-    /**
-     * Deletes a record based on the primary key value.  Does nothing if no
-     * primary key value has been set.
-     *
-     * @return $this
-     */
-    public function delete();
-
-    /**
-     * Manually set the primary key value
+     * Set the primary key value for this object
      *
      * @param mixed $value
      *
      * @return $this
      */
-    public function setId($value);
+    public function setId($value)
+    {
+        $pkField = $this->getPrimaryKeyField();
+
+        if ( $pkField !== null )
+        {
+            $this->set($pkField, $value);
+        }
+
+        return $this;
+    }
 
     /**
-     * Fetch the primary key value of this object, or NULL if not set.
+     * Provide the primary key value
      *
      * @return mixed|null
      */
-    public function getId();
+    public function getId()
+    {
+        $rtn = null;
+
+        $pkField = $this->getPrimaryKeyField();
+
+        if ( $pkField !== null )
+        {
+            $rtn = $this->get($pkField);
+        }
+
+        return $rtn;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function save()
+    {
+        if ( $this->getLoadStatus() == self::NOT_LOADED )
+        {
+            $this->insertRecord();
+        }
+
+        if ( $this->getLoadStatus() == self::LOADED )
+        {
+            $this->updateRecord();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function load($primaryKeyValue = null)
+    {
+        $db = $this->_objDb;
+
+        $tableName  = $this->_objTable->getName();
+        $primaryKey = $this->_objTable->getPrimaryKeys()[0];
+
+        if ( $primaryKeyValue == null )
+        {
+            $id = $this->getId();
+        }
+        else
+        {
+            $id = $primaryKeyValue;
+        }
+
+        if ( $id == null )
+        {
+            throw new \UnderflowException('No primary key value specified. Unable to load');
+        }
+
+        $sql = $this->getSqlDriver()->select()
+                    ->from( $tableName )
+                    ->where( $primaryKey . ' = ?', $id);
+
+        $statement = $db->prepare($sql->output());
+        $statement->execute($sql->getBindings());
+
+        if ( $statement->rowCount() == 0 )
+        {
+            $this->_objData = [];
+            $this->_objLoadStatus = self::NOT_FOUND;
+
+            return $this;
+        }
+
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+
+        foreach ( $result as $field => $value )
+        {
+            $this->set($field, $value);
+        }
+
+        $this->setId( $this->get($this->getPrimaryKeyField()) );
+
+        $this->_objLoadStatus = self::LOADED;
+
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function loadFromWhere($where, $binding = null)
+    {
+        if ( $binding !== null and !is_array($binding) )
+        {
+            $binding = [$binding];
+        }
+
+        $db = $this->_objDb;
+
+        $tableName  = $this->_objTable->getName();
+
+        $sql = $this->getSqlDriver()
+            ->select()
+            ->from( $tableName );
+
+        if ( !empty($binding) )
+        {
+            $sql->where($where, $binding);
+        }
+        else
+        {
+            $sql->where($where);
+        }
+
+        $sql->limit(1);
+
+        $statement = $db->prepare($sql->output());
+        $statement->execute($sql->getBindings());
+
+        if ( $statement->rowCount() == 0 )
+        {
+            $this->_objData = [];
+            $this->_objLoadStatus = self::NOT_FOUND;
+
+            return $this;
+        }
+
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+
+        foreach ( $result as $field => $value )
+        {
+            $this->set($field, $value);
+        }
+
+        $this->setId( $this->get($this->getPrimaryKeyField()) );
+
+        $this->_objLoadStatus = self::LOADED;
+
+        return $this;
+    }
+
+    /**
+     * Provide the load status of the object based on the constants of the
+     * interface.
+     *
+     * @return integer
+     */
+    public function getLoadStatus()
+    {
+        return $this->_objLoadStatus;
+    }
+
+    /**
+     * Delete the loaded record from the database.
+     * Does nothing if no record is loaded.
+     *
+     * @return $this
+     */
+    public function delete()
+    {
+        if ( $this->getLoadStatus() !== self::LOADED )
+        {
+            return $this;
+        }
+
+        return $this;
+    }
 
     /**
      * Provide the database connection used for this item
      *
-     * @return \PDO
+     * @return PDO
      */
-    public function getDb();
+    public function getDb()
+    {
+        return $this->_objDb;
+    }
 
     /**
-     * Provide the SQL Driver used to create the queries for this item
-     *
-     * @return DBSql\DriverInterface
-     *
-     * @throws \UnexpectedValueException When no engine is found
-     */
-    public function getSqlDriver();
-
-    /**
-     * Provide the database table that was set in this object
+     * Provide the database table to be used for this DB Item
      *
      * @return DBTable
      */
-    public function getDBTable();
+    public function getDBTable()
+    {
+        return $this->_objTable;
+    }
+
+    /**
+     * Provide the SQL Driver based on the type of DBTable provided
+     *
+     * @return DBSql\DriverInterface
+     *
+     * @throws \UnexpectedValueException  When no engine is found
+     */
+    public function getSqlDriver()
+    {
+        if ( $this->_objTable instanceof DBTable\PostgreSQL )
+        {
+            return DBSql::PostgreSQL();
+        }
+
+        throw new \UnexpectedValueException('Unsupported SQL Engine Requested');
+    }
+
+    /**
+     * Insert a new record from the data in this object
+     *
+     */
+    protected function insertRecord()
+    {
+        $fetchKey    = false;
+        $primaryKeys = $this->getDBTable()->getPrimaryKeys();
+        $fields      = $this->getDBTable()->getFields();
+
+        $insert = $this->getSqlDriver()->insert();
+        $insert->table($this->getDBTable()->getFQN());
+        $insData = array();
+
+        foreach ( $fields as $field )
+        {
+            $fieldName = $field->getName();
+
+            if ( $this->__isset($fieldName) )
+            {
+                $value = $field->getSqlBoundValue($this->get($fieldName));
+                $insData[ $fieldName ] = $value;
+            }
+        }
+
+        // Nothing to insert?  Time to leave.
+        if ( empty($insData) )
+        {
+            return;
+        }
+
+        $insert->fieldValues($insData);
+
+        if ( $this->_objTable instanceof DBTable\PostgreSQL )
+        {
+            if ( !empty($primaryKeys) )
+            {
+                foreach ( $primaryKeys as $primaryKey )
+                {
+                    $insert->returning($primaryKey);
+                    $fetchKey = true;
+                }
+            }
+        }
+
+        $statement = $this->getDb()->prepare($insert->output());
+        $statement->execute($insert->getBindings());
+
+        if ( $fetchKey )
+        {
+            $result = $statement->fetch(PDO::FETCH_ASSOC);
+
+            foreach ( $primaryKeys as $primaryKey )
+            {
+                $this->set($primaryKey, $result[$primaryKey]);
+            }
+        }
+    }
+
+    /**
+     * Update an existing record
+     *
+     */
+    protected function updateRecord()
+    {
+        $primaryKeys = $this->getDBTable()->getPrimaryKeys();
+
+        // Cannot update a record without a primary key
+        if ( empty($primaryKeys) )
+        {
+            return;
+        }
+
+        $update = $this->getSqlDriver()->update();
+
+        $update->table($this->getDBTable()->getFQN());
+
+        foreach ( $this->_objData as $fieldName => $value )
+        {
+            // Primary keys do not be updated here.  They're criteria for what
+            // will be updated.
+            if ( in_array($fieldName, $primaryKeys) )
+            {
+                continue;
+            }
+
+            // The field must exist in the table
+            if ( !$this->getDBTable()->fieldExists($fieldName) )
+            {
+                continue;
+            }
+
+            $bindValue = $this->getDBTable()
+                ->getField($fieldName)
+                ->getSqlBoundValue($value);
+
+            $update->fieldValue($fieldName, '?', $bindValue);
+        }
+
+        foreach ( $primaryKeys as $primaryKey )
+        {
+            $bindValue = $this->getDBTable()->getField($primaryKey)
+                ->getSqlBoundValue( $this->get($primaryKey));
+
+            $update->where($primaryKey . ' = ?', $bindValue);
+        }
+
+        $statement = $this->_objDb->prepare($update->output());
+        $statement->execute($update->getBindings());
+    }
 }
