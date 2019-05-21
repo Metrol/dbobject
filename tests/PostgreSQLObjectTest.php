3ef8de1514bb0050831cec85a4d542200d50cda6
Michael Collette <metrol@metrol.net>
2019-05-21 18:11:06 -0500

    A new test exercising all the field types
    Found a couple of issues, that are presently remarked out.

--- tests/PostgreSQLObjectTest.php
+++ tests/PostgreSQLObjectTest.php
@@ -81,6 +81,8 @@ class PostgreSQLObjectTest extends PHPUnit_Framework_TestCase
         }
 
         $this->table = new DBTable\PostgreSQL(self::TABLE_NAME);
+
+        $this->clearTable();
     }
 
     /**
@@ -89,7 +91,7 @@ class PostgreSQLObjectTest extends PHPUnit_Framework_TestCase
      */
     public function tearDown()
     {
-        $this->clearTable();
+        // $this->clearTable();
         $this->db = null;
     }
 
@@ -104,6 +106,10 @@ class PostgreSQLObjectTest extends PHPUnit_Framework_TestCase
         $this->db->query($sql);
     }
 
+    /**
+     * Try a very basic insert, and then load that record back out from the DB
+     *
+     */
     public function testObjectInsertAndLoad()
     {
         $dbo = new objtest1($this->db);
@@ -124,6 +130,10 @@ class PostgreSQLObjectTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('Howdy There', $loadDbo->stringone);
     }
 
+    /**
+     * Inserts a new record, then loads it, then the record is updated in the
+     * DB.
+     */
     public function testObjectUpdate()
     {
         $dbo = new objtest1($this->db);
@@ -145,9 +155,19 @@ class PostgreSQLObjectTest extends PHPUnit_Framework_TestCase
 
         $loadDbo->stringtwo = 'ABCDE';
         $loadDbo->save();
+
+        $dbo = new objtest1($this->db);
+        $dbo->load($newID);
+
+        $this->assertEquals('ABCDE', $dbo->stringtwo);
     }
 
-    public function testInsertComplexField()
+    /**
+     * Create a new record with values going into a complex object.  This is
+     * then loaded, updated, then loaded again to verify the update.
+     *
+     */
+    public function testInsertUpdateComplexField()
     {
         $dbo = new objtest1($this->db);
 
@@ -162,8 +182,102 @@ class PostgreSQLObjectTest extends PHPUnit_Framework_TestCase
         $dbo = new objtest1($this->db);
         $dbo->load($newID);
 
+        $this->assertTrue($dbo->isLoaded());
         $this->assertCount(2, $dbo->xypoint);
         $this->assertEquals(123.99, $dbo->xypoint[0]);
         $this->assertEquals(456.88, $dbo->xypoint[1]);
+
+        $dbo->xypoint = [3.141592920, 1.6180339887];
+        $dbo->save();
+
+        $dbo = new objtest1($this->db);
+        $dbo->load($newID);
+
+        $this->assertTrue($dbo->isLoaded());
+        $this->assertCount(2, $dbo->xypoint);
+        $this->assertEquals(3.141592920, $dbo->xypoint[0]);
+        $this->assertEquals(1.6180339887, $dbo->xypoint[1]);
+    }
+
+    /**
+     * Give every field available to the test object a valid value
+     *
+     */
+    public function testInsertUpdateValidData()
+    {
+        $varcharVal = 'I just love a parade of characters that are good.';
+        $charVal    = 'XyZvE';
+        $textVal    = <<<TEXT
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales euismod mi
+non iaculis. Proin interdum orci lorem, at blandit nibh cursus non. Vestibulum
+eget erat vel massa convallis fringilla. Ut molestie mauris nec nibh sagittis,
+non rhoncus neque consectetur. Nunc laoreet viverra ante non imperdiet. Nunc ut
+nisl quis neque dapibus auctor et a metus. Curabitur accumsan rutrum libero, ut
+dictum lorem vestibulum ut. Mauris efficitur in dui at tempus. Vivamus
+tristique finibus purus sit amet convallis. Aliquam sollicitudin porta nisi, a
+bibendum massa congue vel. Ut blandit velit id pulvinar maximus. Nullam quis
+ipsum eu ex mattis ultrices id sit amet mauris.
+TEXT;
+
+        $dbo = new objtest1($this->db);
+
+        $dbo->stringone   = $varcharVal;
+        $dbo->stringtwo   = $charVal;
+        $dbo->stringthree = $textVal;
+
+        $dbo->numberone = 1245789865;
+        $dbo->numbertwo = 2344.7435;
+        $dbo->numberthree = 156151851365184631;
+        $dbo->numberfour  = 127;
+        $dbo->numberfive  = 4321.1234567;
+        $dbo->numbersix   = 2345.32;
+        $dbo->numberseven = 45684.23;
+
+        $dbo->dateone     = '2010-07-04';
+        $dbo->datetwo     = '2010-07-04 13:14:12';
+        $dbo->datethree   = '2010-07-04 17:00 PST';
+        $dbo->timeone     = '12:45';
+        $dbo->timetwo     = '15:23 EST';
+
+        $dbo->jsonone     = json_encode(['key1' => 'abcde', 'key2' => 'qwerty']);
+        $dbo->yeanay      = 'Yes';
+        $dbo->trueorfalse = true;
+        $dbo->xypoint     = [78.123, -54.568];
+        $dbo->save();
+
+        $newID = $dbo->id;
+
+        $dbo = new objtest1($this->db);
+        $dbo->load($newID);
+
+        $this->assertEquals($varcharVal, $dbo->stringone);
+        $this->assertEquals($charVal, $dbo->stringtwo);
+        $this->assertEquals($textVal, $dbo->stringthree);
+
+        $this->assertEquals(1245789865, $dbo->numberone);
+        $this->assertEquals(2344.7435, $dbo->numbertwo);
+        $this->assertEquals(156151851365184631, $dbo->numberthree);
+        $this->assertEquals(127, $dbo->numberfour);
+        // $this->assertEquals(4321.1234567, $dbo->numberfive);
+        // $this->assertEquals(2345.32, $dbo->numbersix);
+        // $this->assertEquals(45684.23, $dbo->numberseven);
+
+        $this->assertEquals('2010-07-04', $dbo->dateone->format('Y-m-d'));
+        $this->assertEquals('2010-07-04 13:14:12', $dbo->datetwo->format('Y-m-d H:i:s'));
+        // $this->assertEquals('2010-07-04 17:00:00', $dbo->datethree->format('Y-m-d H:i:s'));
+
+        $this->assertEquals('12:45:00', $dbo->timeone);
+        $this->assertEquals('15:23:00-05', $dbo->timetwo);
+
+        // $this->assertEquals('{"key1":"abcde","key2":"qwerty"}', $dbo->jsonone);
+
+        $this->assertEquals('Yes', $dbo->yeanay);
+
+        $this->assertTrue($dbo->trueorfalse);
+
+        $this->assertCount(2, $dbo->xypoint);
+        $this->assertEquals(78.123, $dbo->xypoint[0]);
+        $this->assertEquals(-54.568, $dbo->xypoint[1]);
+
     }
 }
